6c6e3bb89202a3f29abe430b1cde31128c6dcd57
Cristian Canedo <hackerexecute@protonmail.com>
2018-10-30 03:10:15 +0100

    Test setting of key and iv upon random generation

--- tests/001-AesCrypto-Set.cpp
+++ tests/001-AesCrypto-Set.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <catch.hpp>
 #include "aescrypto.h"
 
@@ -32,42 +33,42 @@ SCENARIO("setting key", "[set][key]")
 
             CHECK(mockIv == actualKey);
         }
+    }
 
-        WHEN("key of greater size is set")
-        {
-            mockIv.CleanNew(mockIv.size() + 8);
-            crypto.setKey(mockIv);
-            actualKey = crypto.getKey();
-            
-            REQUIRE(mockIv == actualKey);
+    WHEN("key of greater size is set")
+    {
+        mockIv.CleanNew(mockIv.size() + 8);
+        crypto.setKey(mockIv);
+        SecByteBlock actualKey = crypto.getKey();
+        
+        REQUIRE(mockIv == actualKey);
 
-            mockIv.CleanNew(mockIv.size() + 8);
-            crypto.setKey(mockIv);
-            actualKey = crypto.getKey();
+        mockIv.CleanNew(mockIv.size() + 8);
+        crypto.setKey(mockIv);
+        actualKey = crypto.getKey();
 
-            REQUIRE(mockIv == actualKey);
-        }
+        REQUIRE(mockIv == actualKey);
+    }
 
-        WHEN("key of lesser size is set")
-        {
-            mockIv.CleanNew(AesCrypto::MAX_KEYLENGTH);
-            crypto.setKey(mockIv);
-            actualKey = crypto.getKey();
-            
-            CHECK(actualKey.size() == AesCrypto::MAX_KEYLENGTH);
+    WHEN("key of lesser size is set")
+    {
+        mockIv.CleanNew(AesCrypto::MAX_KEYLENGTH);
+        crypto.setKey(mockIv);
+        SecByteBlock actualKey = crypto.getKey();
+        
+        CHECK(actualKey.size() == AesCrypto::MAX_KEYLENGTH);
 
-            mockIv.CleanNew(mockIv.size() - 8);
-            crypto.setKey(mockIv);
-            actualKey = crypto.getKey();
+        mockIv.CleanNew(mockIv.size() - 8);
+        crypto.setKey(mockIv);
+        actualKey = crypto.getKey();
 
-            REQUIRE(mockIv == actualKey);
+        REQUIRE(mockIv == actualKey);
 
-            mockIv.CleanNew(mockIv.size() - 8);
-            crypto.setKey(mockIv);
-            actualKey = crypto.getKey();
+        mockIv.CleanNew(mockIv.size() - 8);
+        crypto.setKey(mockIv);
+        actualKey = crypto.getKey();
 
-            REQUIRE(mockIv == actualKey);
-        }
+        REQUIRE(mockIv == actualKey);
     }
 }
 
@@ -89,4 +90,29 @@ SCENARIO("setting iv", "[set][iv]")
     {
         REQUIRE_NOTHROW(crypto.setIv(mockIv));
     }
+}
+
+SCENARIO("generating a random key or iv sets them to the instance of AesCrypto", "[set][generate]")
+{
+    AesCrypto aes;
+
+    // 24 byte key and 16 byte iv
+    SecByteBlock myKey = aes.toByteBlock("My distinguishable key!!");
+    SecByteBlock myIv = aes.toByteBlock("My unique iv!!!!");
+    aes.setKey(myKey);
+    aes.setIv(myIv);
+
+    // Make sure our unique key and iv are set
+    CHECK(aes.getKey() == myKey);
+    CHECK(aes.getIv() == myIv);
+
+    // Randomly generating key and iv should overwrite what is set
+    aes.generateKey();
+    aes.generateIv();
+
+    SecByteBlock currentKey = aes.getKey();
+    SecByteBlock currentIv = aes.getIv();
+
+    REQUIRE_FALSE(myKey == currentKey);
+    REQUIRE_FALSE(myIv == currentIv);
 }
